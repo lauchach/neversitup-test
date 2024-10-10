600ce2e19426193313c36339d047107310dbb081
lauchach <tarnapot.b38@gmail.com>
2024-10-10 09:03:10 +0700

    [edit] frist commit

---
+++ javascript/manipulate.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {string} text string of value example "aabb", "abcde"
+ * @returns {string[]} string array of result
+ */
+const manipulate = (text) => {
+    // TODO : start your code here
+}
+
+module.exports = {
+    manipulate
+}
---
+++ javascript/smily.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {string[]} texts list of string [":)", ":(", ":>"]
+ * @returns {number} amount of smily face detected
+ */
+const countSmilyFace = (text) => {
+    // TODO : start your code here
+}
+
+module.exports = {
+    countSmilyFace
+}
